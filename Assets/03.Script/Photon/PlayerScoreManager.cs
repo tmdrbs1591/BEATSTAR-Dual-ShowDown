6d591e6faed188251a02b3b06d3474a150867f6e
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-10-15 23:22:54 +0900

    완성 | 멀티플레이 기본 틀 완성

--- Assets/03.Script/Photon/PlayerScoreManager.cs
+++ Assets/03.Script/Photon/PlayerScoreManager.cs
@@ -10,8 +10,11 @@ public class PlayerScoreManager : MonoBehaviourPunCallbacks
     public List<PlayerScore> playerScores = new List<PlayerScore>();
     public List<ScoreListItem> playerScoreLists = new List<ScoreListItem>();
 
-    public GameObject WinnerPanel;
-    public GameObject LosePanel;
+    public GameObject firstPlacePanel;
+    public GameObject secondPlacePanel;
+    public GameObject thirdPlacePanel;
+    public GameObject fourthPlacePanel;
+
 
     private void Awake()
     {
@@ -34,48 +37,37 @@ public class PlayerScoreManager : MonoBehaviourPunCallbacks
     public void WinnerLoserPanel()
     {
         // 현재 가장 높은 점수와 해당 플레이어를 추적
-        PlayerScore highestScorePlayer = null;
-        PlayerScore lowestScorePlayer = null;
-        float highestScore = float.MinValue;
-        float lowestScore = float.MaxValue;
+        List<PlayerScore> sortedScores = new List<PlayerScore>(playerScores);
 
-        // 최고 점수와 최저 점수 찾기
-        foreach (var playerScore in playerScores)
+        // 점수를 내림차순으로 정렬
+        sortedScores.Sort((score1, score2) => score2.currentScore.CompareTo(score1.currentScore));
+
+        // 패널 비활성화
+        firstPlacePanel.SetActive(false);
+        secondPlacePanel.SetActive(false);
+        thirdPlacePanel.SetActive(false);
+        fourthPlacePanel.SetActive(false);
+
+        // 1등, 2등, 3등, 4등에 맞는 패널 활성화 (IsMine을 통해 현재 플레이어에게만 적용)
+        if (sortedScores.Count > 0 && sortedScores[0].photonView.IsMine)
         {
-            if (playerScore.currentScore > highestScore)
-            {
-                highestScore = playerScore.currentScore;
-                highestScorePlayer = playerScore;
-            }
-
-            if (playerScore.currentScore < lowestScore)
-            {
-                lowestScore = playerScore.currentScore;
-                lowestScorePlayer = playerScore;
-            }
+            firstPlacePanel.SetActive(true);  // 1등 패널
+            Debug.Log($"1등: {sortedScores[0].photonView.Owner.NickName} - 점수: {sortedScores[0].currentScore}");
         }
-
-        // 모든 패널 비활성화
-        if (WinnerPanel != null)
+        if (sortedScores.Count > 1 && sortedScores[1].photonView.IsMine)
         {
-            WinnerPanel.SetActive(false);
+            secondPlacePanel.SetActive(true);  // 2등 패널
+            Debug.Log($"2등: {sortedScores[1].photonView.Owner.NickName} - 점수: {sortedScores[1].currentScore}");
         }
-
-        if (LosePanel != null)
+        if (sortedScores.Count > 2 && sortedScores[2].photonView.IsMine)
         {
-            LosePanel.SetActive(false);
+            thirdPlacePanel.SetActive(true);  // 3등 패널
+            Debug.Log($"3등: {sortedScores[2].photonView.Owner.NickName} - 점수: {sortedScores[2].currentScore}");
         }
-
-        // 최고 점수 플레이어에게만 WinnerPanel 활성화 (IsMine을 통해 현재 플레이어에게만 적용)
-        if (highestScorePlayer != null && highestScorePlayer.photonView.IsMine && WinnerPanel != null)
+        if (sortedScores.Count > 3 && sortedScores[3].photonView.IsMine)
         {
-            WinnerPanel.SetActive(true);
-        }
-
-        // 최저 점수 플레이어에게만 LosePanel 활성화 (IsMine을 통해 현재 플레이어에게만 적용)
-        if (lowestScorePlayer != null && lowestScorePlayer.photonView.IsMine && LosePanel != null)
-        {
-            LosePanel.SetActive(true);
+            fourthPlacePanel.SetActive(true);  // 4등 패널
+            Debug.Log($"4등: {sortedScores[3].photonView.Owner.NickName} - 점수: {sortedScores[3].currentScore}");
         }
     }
     public void DisplayScoreRankings()
