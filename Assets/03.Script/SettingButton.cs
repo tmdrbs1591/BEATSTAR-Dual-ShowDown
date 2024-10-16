27265f8c393188802765e5fab8763fefd73d57e3
tmdrbs1591 <sdh230211@sdh.hs.kr>
2024-10-16 23:55:55 +0900

    추가 | 스테이지모드 노래추가

--- Assets/03.Script/SettingButton.cs
+++ Assets/03.Script/SettingButton.cs
@@ -26,16 +26,23 @@ public class SettingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     private Vector3 originalPosition;
     private void Awake()
     {
-        // Awake에서 초기 위치를 저장
-        originalPosition = transform.position;
+        if (Type != "None")
+        {
+            // Awake에서 초기 위치를 저장
+            originalPosition = transform.position;
+        }
     }
 
     private void OnEnable()
     {
-        // 위치를 초기화하고 애니메이션을 시작
-        transform.position = originalPosition;
-        StartCoroutine(AnimateButton());
-          // 기본 메테리얼로 교체합니다.
+        if (Type != "None")
+        {
+            // 위치를 초기화하고 애니메이션을 시작
+            transform.position = originalPosition;
+     
+            StartCoroutine(AnimateButton());
+        }
+        // 기본 메테리얼로 교체합니다.
         buttonImage.material = originalMaterial;
     }
     void Start()
@@ -47,6 +54,7 @@ public class SettingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 
     IEnumerator AnimateButton()
     {
+    
         // 버튼의 초기 위치를 오른쪽으로 이동시킵니다.
         Vector3 startPos = originalPosition + new Vector3(20, 0, 0); // 300은 임의의 값입니다.
         transform.position = startPos;
@@ -62,13 +70,16 @@ public class SettingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
             if (Type == "MenuButton")
                 AudioManager.instance.PlaySound(transform.position, 3, Random.Range(1.2f, 1.2f), 1);
             else
-                AudioManager.instance.PlaySound(transform.position, 12, Random.Range(1f, 1f), 1);
+                AudioManager.instance.PlaySound(transform.position, 3, Random.Range(1f, 1f), 1);
             // 마우스가 버튼 위에 있을 때 아웃라인 메테리얼로 교체합니다.
             buttonImage.material = outlineMaterial;
 
-            transform.DOMoveX(originalPosition.x - 1f, 0.2f).SetEase(Ease.OutSine); // 왼쪽으로 이동하는 애니메이션 추가
-            // 추가적인 애니메이션 등을 실행할 수 있습니다.
-            transform.DOScale(scaleUp, scaleDuration).SetEase(Ease.OutSine);
+            if (Type != "None")
+            {
+                transform.DOMoveX(originalPosition.x - 1f, 0.2f).SetEase(Ease.OutSine); // 왼쪽으로 이동하는 애니메이션 추가
+                                                                                        // 추가적인 애니메이션 등을 실행할 수 있습니다.
+                transform.DOScale(scaleUp, scaleDuration).SetEase(Ease.OutSine);
+            }
         }
     }
 
@@ -78,9 +89,12 @@ public class SettingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
         {
             // 마우스가 버튼에서 벗어날 때 기본 메테리얼로 교체합니다.
             buttonImage.material = originalMaterial;
-            transform.DOMoveX(originalPosition.x, 0.2f).SetEase(Ease.OutSine); // 초기 위치로 되돌아가는 애니메이션 추가
-            // 추가적인 애니메이션 등을 실행할 수 있습니다.
-            transform.DOScale(Vector3.one, scaleDuration).SetEase(Ease.OutSine);
+            if (Type != "None")
+            {
+                transform.DOMoveX(originalPosition.x, 0.2f).SetEase(Ease.OutSine); // 초기 위치로 되돌아가는 애니메이션 추가
+                                                                                   // 추가적인 애니메이션 등을 실행할 수 있습니다.
+                transform.DOScale(Vector3.one, scaleDuration).SetEase(Ease.OutSine);
+            }
         }
     }
 
@@ -88,22 +102,29 @@ public class SettingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
     {
         isNavigated = true;
 
-        // 네비게이션으로 버튼이 선택됐을 때의 처리
-        transform.DOMoveX(originalPosition.x - 1f, 0.2f).SetEase(Ease.OutSine); // 왼쪽으로 이동하는 애니메이션 추가
+        if (Type != "None")
+        {
+            // 네비게이션으로 버튼이 선택됐을 때의 처리
+            transform.DOMoveX(originalPosition.x - 1f, 0.2f).SetEase(Ease.OutSine); // 왼쪽으로 이동하는 애니메이션 추가
+        }
         buttonImage.material = outlineMaterial;
         if (Type == "MenuButton")
             AudioManager.instance.PlaySound(transform.position, 3, Random.Range(1.2f, 1.2f), 1);
         else
-            AudioManager.instance.PlaySound(transform.position, 12, Random.Range(1f, 1f), 1);
+            AudioManager.instance.PlaySound(transform.position, 3, Random.Range(1f, 1f), 1);
     }
 
     public void OnDeselect(BaseEventData eventData)
     {
         isNavigated = false;
 
-        // 네비게이션에서 버튼이 선택 해제됐을 때의 처리
-        transform.DOMoveX(originalPosition.x, 0.2f).SetEase(Ease.OutSine); // 초기 위치로 되돌아가는 애니메이션 추가
         buttonImage.material = originalMaterial;
+
+        if (Type != "None")
+        {
+            // 네비게이션에서 버튼이 선택 해제됐을 때의 처리
+            transform.DOMoveX(originalPosition.x, 0.2f).SetEase(Ease.OutSine); // 초기 위치로 되돌아가는 애니메이션 추가
+        }
     }
 
     void Update()
@@ -113,26 +134,29 @@ public class SettingButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHa
 
     public void NavLock()
     {
-        if (buttonManager.isNavimpossible && Type == "MenuButton" || buttonManager.isCharPanel && Type == "MenuButton" || buttonManager.isTitleSettingPanel && Type == "MenuButton")
+        if (Type != "None")
         {
-            var navigation = new Navigation();
-            navigation.mode = Navigation.Mode.None;
+            if (buttonManager.isNavimpossible && Type == "MenuButton" || buttonManager.isCharPanel && Type == "MenuButton" || buttonManager.isTitleSettingPanel && Type == "MenuButton")
+            {
+                var navigation = new Navigation();
+                navigation.mode = Navigation.Mode.None;
 
-            menuBtn.navigation = navigation;
-        }
-        else if (Type == "SettingButton")
-        {
-            var navigation = new Navigation();
-            navigation.mode = Navigation.Mode.Vertical;
+                menuBtn.navigation = navigation;
+            }
+            else if (Type == "SettingButton")
+            {
+                var navigation = new Navigation();
+                navigation.mode = Navigation.Mode.Vertical;
 
-            menuBtn.navigation = navigation;
-        }
-        else
-        {
-            var navigation = new Navigation();
-            navigation.mode = Navigation.Mode.Horizontal;
+                menuBtn.navigation = navigation;
+            }
+            else
+            {
+                var navigation = new Navigation();
+                navigation.mode = Navigation.Mode.Horizontal;
 
-            menuBtn.navigation = navigation;
+                menuBtn.navigation = navigation;
+            }
         }
     }
 }
